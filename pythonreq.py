b0236ea4f1ef4a033771c8621d6f159026ac2739
mareic <mareic.kilolo@bt.com>
2021-05-24 12:44:16 +0100

    Added JWT authentication

--- pythonreq.py
+++ pythonreq.py
@@ -6,34 +6,55 @@ homePage = "http://127.0.0.1:18080/"
 home = requests.get(homePage)
 print(home)
 
-def login():
-    again = True;
-    loginPage = homePage + "login"
-    while(again == True):
-        username = input("Enter your username: ")
-        password = input("Enter password: ")
-        resp = requests.post(loginPage, auth=HTTPBasicAuth(username, password))
-        #print(resp.status_code)
-        if(resp.status_code == 401):
-            print("Account doesn't exist, your're not authorised to login.\n")
-            print("Please try again")
-        else:
-            print("Welcome\n");
-            again = False;
+class User:
+    username = ""#These are properties of the class
+    password = ""
+    header = ""
 
-     #print(resp.url)
+#below the method is a function that belongs to a class
+    def login(self):
+        again = True
+        loginPage = homePage + "login"
+        while(again == True):
+            self.username = input("Please enter your username: ")
+            self.password = input("Please enter your password: ")
+            username = "%s" % (self.username)
+            password = "%s" % (self.password)
+            resp = requests.post(loginPage, auth=HTTPBasicAuth(username, password))
+            if(resp.status_code == 401):
+                print("Account doesn't exist, your're not authorised to login.")
+                print("Please try again")
+            else:
+                print("Welcome")
+                self.header = resp.headers
+                #print(resp.headers)
+                again = False
+        #return resp.headers
 
-def readMessages(user):
+def readMessages(user, header):
     url = homePage + "user/" + user
-    print(url)
-    r = requests.get(url)
-    jsonList = r.json()['Messages']
-    
+    resp = requests.get(url, headers={'Authorization':header['JWT']})
+    #resp = requests.get(url, headers={'Authorization':"I am a bad person Tom"})
+    #print(url)
+    #r = requests.get(url)
+    #print(resp.headers)
+    #print(resp.content)
+
+    try:
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        if(resp.status_code == 401):
+            print(err)
+            return
+        else:
+            raise
+
+    jsonList = resp.json()['Messages']  
     for num, messages in enumerate(jsonList, start=1):
         print("Message number {} is: {} ".format(num, messages));
         input()
 
-def writeMessages():
+def writeMessages(user, header):
     sendMessage = input("Message: ")
     #r = requests.get(url)
     #jsonList = r.json()['Messages']
@@ -41,17 +62,16 @@ def writeMessages():
     
     #jsonList.append(sendMessage)
     print(jsonList)
-    p = requests.post("http://127.0.0.1:18080/user/Mareic1073/send_messages", json=jsonList)
+    sendMessUrl = "http://127.0.0.1:18080/user/" + user + "/send_messages"
+    #p = requests.post(sendMessUrl, json=jsonList, headers={'Authorization':"I am hacker"})
+    p = requests.post(sendMessUrl, json=jsonList, headers={'Authorization':header['JWT']})
     print(p.text)
 
-login()
+user1 = User()
+user1.login()
 
 readOrWrite = input("Would you like to read or write a message? ")
 if(readOrWrite == 'Read' or readOrWrite == 'read'):
-    readMessages(username)
+    readMessages(user1.username, user1.header)
 elif(readOrWrite == 'Write' or readOrWrite == 'write'):
-    writeMessages()
-    
-
-
-
+    writeMessages(user1.username, user1.header)
--- server.cpp
+++ server.cpp
@@ -4,6 +4,8 @@ using std::cin;
 #include <string>
 using std::string;
 #include <sstream>
+#include <fstream>
+#include <map>
 #include <vector>
 #include "crow_all.h"
 #include "sqliteDatabase.h"
@@ -48,6 +50,8 @@ string writeMessages() {
 int main(int argc, char* argv[]){
 	//string senderName = "Mareic1073";
 	string receiverName = "Emre26995";
+	std::map<string, string> key{};
+
 	
 	crow::SimpleApp app;
 	
@@ -56,14 +60,14 @@ int main(int argc, char* argv[]){
 		return "<div><h1>Hello, Welcome to the chat server.<h1><div>";
 	});
 
-	CROW_ROUTE(app, "/login").methods("POST"_method)([](const crow::request& req){
+	CROW_ROUTE(app, "/login").methods("POST"_method)([&key](const crow::request& req){
 			//This is where you will decode the username
 			//Check if it exist in the database
 			string authValue = req.get_header_value("Authorization");
-			printf("%s\n",authValue.c_str());
+			//printf("%s\n",authValue.c_str());
 			int spaces = authValue.find(" ");
 			string pass = base64_decode(authValue.substr(spaces + 1), false);
-			cout << pass << "\n";
+			//cout << pass << "\n";
 
 			int colonLoc = pass.find(":");
 			string userName = pass.substr(0,colonLoc);
@@ -71,52 +75,33 @@ int main(int argc, char* argv[]){
 			//cout << userName << " " << passWord << "\n";
 			bool results = login_check(userName, passWord);
 			int result = (int)results;
-			printf("%d\n", results);
+			//printf("%d\n", results);
 			if(results == false){
 				return crow::response(401);
 			}
-			string keyPub = R"(AAAAB3NzaC1yc2EAAAADAQABAAABgQDRpv/FkkTkkc3dGrfUK8zOHRsH+r35fFadn+MR3w/T1PDHDg+1P+s8LcQdZDPZP7nbv0hbB58xh2TmjgJF0m+DHdyLMlYG9FTV+aVCYpZysReTFDwug2gUblBPtPHiga5IlfctOewVjv8COptTzLmaeLXFUOmmsDKuTRvTexa6KYW7rhTvVi/uL1tz1Pmte8B+baAgyZLfAn61LwQrWKaDzKTAhcIS79Hyyi07Y1yP/QQf+CTe1NcqLXxQDuGc/2JHTPudRwCT7Gf3gFGF+ATdx0yF3ju1JrToTCKfsMIv2tE6i4ZxS9tp4o157dXPR/14lJr3Ac+mLtR2mL/WW+V/GUR1xhuc6Ysit2V0mlbhsOfEMUyzjsKs+tBlYYwwKpxM7VR9+RxFJjD8GphdailOfa2T8Q2isuJiv5pXmgbzmMAiCgwRRyzylDVYbFsMz8f1MpkMJ411Etc47OH3C4YUTdgWoQ24XCem146Y69xiDklE2f1Cx3iMm5y4OUjBe7s=)";
 
-			string keyPrivate = R"(b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
-NhAAAAAwEAAQAAAYEA0ab/xZJE5JHN3Rq31CvMzh0bB/q9+XxWnZ/jEd8P09Twxw4PtT/r
-PC3EHWQz2T+5279IWwefMYdk5o4CRdJvgx3cizJWBvRU1fmlQmKWcrEXkxQ8LoNoFG5QT7
-Tx4oGuSJX3LTnsFY7/AjqbU8y5mni1xVDpprAyrk0b03sWuimFu64U71Yv7i9bc9T5rXvA
-fm2gIMmS3wJ+tS8EK1img8ykwIXCEu/R8sotO2Ncj/0EH/gk3tTXKi18UA7hnP9iR0z7nU
-cAk+xn94BRhfgE3cdMhd47tSa06Ewin7DCL9rROouGcUvbaeKNee3Vz0f9eJSa9wHPpi7U
-dpi/1lvlfxlEdcYbnOmLIrdldJpW4bDnxDFMs47CrPrQZWGMMCqcTO1UffkcRSYw/BqYXW
-opTn2tk/ENorLiYr+aV5oG85jAIgoMEUcs8pQ1WGxbDM/H9TKZDCeNdRLXOOzh9wuGFE3Y
-FqENuFwnpteOmOvcYg5JRNn9Qsd4jJucuDlIwXu7AAAFmHSixLp0osS6AAAAB3NzaC1yc2
-EAAAGBANGm/8WSROSRzd0at9QrzM4dGwf6vfl8Vp2f4xHfD9PU8McOD7U/6zwtxB1kM9k/
-udu/SFsHnzGHZOaOAkXSb4Md3IsyVgb0VNX5pUJilnKxF5MUPC6DaBRuUE+08eKBrkiV9y
-057BWO/wI6m1PMuZp4tcVQ6aawMq5NG9N7FrophbuuFO9WL+4vW3PU+a17wH5toCDJkt8C
-frUvBCtYpoPMpMCFwhLv0fLKLTtjXI/9BB/4JN7U1yotfFAO4Zz/YkdM+51HAJPsZ/eAUY
-X4BN3HTIXeO7UmtOhMIp+wwi/a0TqLhnFL22nijXnt1c9H/XiUmvcBz6Yu1HaYv9Zb5X8Z
-RHXGG5zpiyK3ZXSaVuGw58QxTLOOwqz60GVhjDAqnEztVH35HEUmMPwamF1qKU59rZPxDa
-Ky4mK/mleaBvOYwCIKDBFHLPKUNVhsWwzPx/UymQwnjXUS1zjs4fcLhhRN2BahDbhcJ6bX
-jpjr3GIOSUTZ/ULHeIybnLg5SMF7uwAAAAMBAAEAAAGBAMcVNwJNIGCQWBWi3nd61DPRja
-6pGydDPBjYEE8TK7tzFOVg4+ewd9egef+S/G1LR1l1rebLOQ9WS6lnpp7Y8o1mmNwRg4F9
-GOZL9vMwHT9yDTncWD2vCPGFw3u9DoiRroc86QgoXZgnPt7F+mO8H2+hOrLRX96qqKW9kT
-PkSbOj5FtRNo4DoOYkUEqRLjrgLe2gehNIdtCQVPQGXoYu24iCltw+6AkFdK+B7yva8bXF
-hNo60ABNyrhBky+Owg1MsDWVCNh8wcI5tLFZGxo6IHxVAzQQzfOsxQHGhxcdtq6y9zC3ko
-gJKVM2Wh6fq5HL0V6/NxciZo36KsP2UStzKDVf+sQDFr7nXrv7PxcC24xIU6tmjtcdugSh
-REuc3cuHtucF66h4iU+wrstHOzAftr+JpksWX0mgEyeYhU9qPlLoxYLZwzqajfVyH5sfVM
-mRSpYyU2r8BBcBbt5T8NACWtYGOg67GPak21todwaOxoAhcw5fNCC2M8KRd9upTGE3iQAA
-AMBUinOMp33bP/IlDMcwJiBtWj5v/Xi4zmV8CqoIGYvtjNEcXiVK8KxEvdRfr+e+xXtjzQ
-XePoU3whM55GK7jfyt9/xnkFcUCGNNAQtIr3RoDKAD2g4LQnQN7H7FF9jdhFICo2SYcmbI
-UER2UozHJc+CQYdU7kHsr7cc5mSYBX0tgZs25PTN6RsGA17wHsfES0TB4din9+pI9r3tAs
-1jAIdXM3z+fxBr/jY14wQsOwkihH53gi7YzBvj8OhV68MGyFUAAADBAOnYGuKR4MD21hv9
-JV2H7R2t+d+1ex3ShnOUea7mkNxBhlvPxZ4sErB/ikHZvhTt0nMYa25NzBez8T63OH8LYJ
-9HzjEZdNcQ25Th5N+W1ABYRpYxrxKdl3OSPNorXJjRXAb7xuKQKUi2FKK+6/mKrCW1E3Wt
-n1WVmbcXNsT4a7bN7VGMZnCnb0fydN85TazGCVYo+3lgsWc99luN+pQ+J+tmO7X50wWBaN
-pYZOF12x6B0vvrJRf1PTqGZyvkaMaRDQAAAMEA5YQe+Ujqbx707bUiVEquVpNTOo932U2v
-/GntIO6IZqZzxcoobkbtZ0K3LgR+Q9rjqTPxm1kVqsUs90TMAG4u9L0zuiV1ugtK3EzsUd
-Kk0RCxDydr+qxYFutMOzwi9f3bqAMKkD/ArArTCHK4RuKTGZ6MtpHgCqIpUoWngjpqFGNs
-SQA8dEbFLH4ue1FzsUXIFaYQhIRyFYzy7roG8xjydwJGUl/X9iyAqHiFmOoU3Yx5P1W/W9
-EOspHjy2NK9r3nAAAAHW1hcmVpY0BtYXJlaWMtVmlydHVhbC1NYWNoaW5lAQIDBAU=)";
+			std::ifstream file1("../jwtrs256.key.pub");
+			std::ifstream file2("../jwtrs256.key");
+			if(file1.is_open()){
+				cout << "File found\n";
+			}
+			else {cout << "Unable to print file content\n";}
+			
+			std::stringstream rsaPublicKey, rsaPrivateKey;
+			rsaPublicKey << file1.rdbuf();
+			rsaPrivateKey << file2.rdbuf();
+			//printf("%s", rsaPublicKey.str().c_str());
 
-			auto token = jwt::create().set_type("JWT").sign(jwt::algorithm::rs256(keyPub, keyPrivate,"",""));
+
+			auto token = jwt::create().set_type("JWT")
+			.set_issuer("chat_server")
+			.set_issued_at(std::chrono::system_clock::now())
+			.sign(jwt::algorithm::rs256(rsaPublicKey.str(), rsaPrivateKey.str(), "", ""));
 			crow::response success = crow::response(200);
-			success.add_header("type", token);
+			success.add_header("JWT", token);
+			key[userName] = token;
+			//cout << "Token: " << key["Mareic1073"] << "\n"; 
+			//cout << "Token: " << token << "\n";
 
 			return success;
 			});
@@ -124,18 +109,61 @@ EOspHjy2NK9r3nAAAAHW1hcmVpY0BtYXJlaWMtVmlydHVhbC1NYWNoaW5lAQIDBAU=)";
 	
 
 
-	CROW_ROUTE(app, "/user/<string>")([receiverName](string senderName){
+	CROW_ROUTE(app, "/user/<string>")([receiverName, &key](const crow::request& req, string senderName){
+			//cout << "Token: " << key[senderName] << "\n";
+			//Recieve header value from client
+			//get it out of the header field
+			//Compare that to senderName
+			//return a 401 if its not right
+			string authValue = req.get_header_value("Authorization");
+			//printf("%s\n", authValue.c_str());
+			auto name = key.find(senderName);
+			string JWToken = key.find(senderName) -> second;
+			if(name == key.end()){
+				cout << "Account not found\n";
+				return crow::response(401);
+			}
+			else{
+				cout << "Account found" << "\n";
+			}
+			//add if statement, if name is not equal to authvalue, return a 401;
+			if(authValue != JWToken){
+				cout << "Token mismatch\n";
+				return crow::response(401);
+			}
+			else{
+				cout << "Token match" << "\n";
+			}
 			crow::json::wvalue x;
 			std::vector<string> m = read_messages(senderName, receiverName);
 			for(int i = 0; i < m.size(); i++){
 				x["Messages"][i] = m.at(i);
-				
-				}
-			return x;
+			}
+			return crow::response(x);
+			
 			});
 
 
-	CROW_ROUTE(app, "/user/<string>/send_messages").methods("POST"_method)([receiverName](const crow::request& req, string senderName){
+	CROW_ROUTE(app, "/user/<string>/send_messages").methods("POST"_method)([receiverName, &key](const crow::request& req, string senderName){
+			string authValue = req.get_header_value("Authorization");
+			//printf("%s\n",authValue);
+			auto name = key.find(senderName);
+			string JWToken = key.find(senderName) -> second;
+			if(name == key.end()){
+				cout << "Account not found\n";
+				return crow::response(401);
+			}
+			else{
+				cout << "Account found\n";
+			}
+
+			if(authValue != JWToken){
+				cout << "Token mismatch\n";
+				return crow::response(401);
+			}
+			else{
+				cout << "Token found\n";
+			}
 			auto x = crow::json::load(req.body);
 			string message;
 			if(!x){
@@ -155,12 +183,6 @@ EOspHjy2NK9r3nAAAAHW1hcmVpY0BtYXJlaWMtVmlydHVhbC1NYWNoaW5lAQIDBAU=)";
 
 
 
-
-	/*CROW_ROUTE(app, "/user")([senderName, recieverName](){
-			crow::json::wvalue x;
-			x[senderName] = read_messages(senderName, recieverName);
-			return x;
-			});*/
 
 	//a crow rout take a username and list/read all the messages for that user.
 	//message_count
